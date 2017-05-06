3f4bc36eb0931d720cd88aece80c986c6eab8cfc
Sean McDermid <fish_boy42@hotmail.com>
2017-05-06 04:44:01 -0600

    fixesa

--- TableauConnector/earthquakeWDC.js
+++ TableauConnector/earthquakeWDC.js
@@ -1,6 +1,8 @@
 ﻿(function () {
+    // Create the connector object
     var myConnector = tableau.makeConnector();
 
+    // Define the schema
     myConnector.getSchema = function (schemaCallback) {
         var cols = [{
             id: "id",
@@ -32,6 +34,7 @@
         schemaCallback([tableSchema]);
     };
 
+    // Download the data
     myConnector.getData = function (table, doneCallback) {
         $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function (resp) {
             var feat = resp.features,
@@ -55,10 +58,11 @@
 
     tableau.registerConnector(myConnector);
 
+    // Create event listeners for when the user submits the form
     $(document).ready(function () {
         $("#submitButton").click(function () {
-            tableau.connectionName = "USGS Earthquake Feed";
-            tableau.submit();
+            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
         });
     });
 })();
